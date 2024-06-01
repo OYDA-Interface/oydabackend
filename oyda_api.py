57ca5459398819bfbc7e8c038a2a0c9d59ba28d7
Oheneba Kwaku Addo Dade <ohenebadade@gmail.com>
2024-06-01 11:13:45 +0000

    change to pg8000

--- oyda_api.py
+++ oyda_api.py
@@ -1,7 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
+
+@app.route("/api/insert_rows", methods=["POST"])
+def insert_rows():
+    if not request.data:
+        return jsonify({"error": "No data provided"})
+    data = json.loads(request.data)
+    # insert data into the postgresql database
 
 
 app.run(debug=True)
